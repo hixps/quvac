44e21981a9ebb23330cf7199c48f7934272bbdce
maxbalrog <maxbalrog@gmail.com>
2025-02-17 10:54:14 +0100

    save ep keys to out file and aff clip for small field values

--- src/quvac/postprocess.py
+++ src/quvac/postprocess.py
@@ -150,7 +150,8 @@ class VacuumEmissionAnalyzer:
         Ex = ne.evaluate("e1x*a1 + e2x*a2", global_dict=self.__dict__)
         Ey = ne.evaluate("e1y*a1 + e2y*a2", global_dict=self.__dict__)
         Ez = ne.evaluate("e1z*a1 + e2z*a2", global_dict=self.__dict__)
-        E = ne.evaluate("sqrt(Ex**2 + Ey**2 + Ez**2)") 
+        E = ne.evaluate("sqrt(Ex**2 + Ey**2 + Ez**2)")
+        E[E < E.max()*1e-10] = 0.
         E_inv = np.nan_to_num(1. / E)
         efx, efy, efz = Ex * E_inv, Ey * E_inv, Ez * E_inv
         return (efx, efy, efz)
@@ -313,7 +314,7 @@ class VacuumEmissionAnalyzer:
         angle_keys = "theta phi beta".split()
         angles = [self.fields_params[self.perp_field_idx][key] for key in angle_keys]
         self.get_perp_signal(angles, perp_type=perp_type)
-        keys = "kx ky kz Np_xyz Np_total".split()
+        keys = "kx ky kz Np_xyz Np_total epx epy epz".split()
 
         if calculate_spherical:
             self.get_signal_on_sph_grid(key="Np_xyz", **spherical_params)
